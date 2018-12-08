7a4babf3b3cde8765a75736b688d40dfa7db79a3
akshit-agarwal-123 <35350686+akshit-agarwal-123@users.noreply.github.com>
2018-12-08 03:20:33 -0700

    Add files via upload

--- Blood Donation.R
+++ Blood Donation.R
@@ -11,144 +11,126 @@ library(ROSE)
 library(lattice)
 library(ggplot2)
 library(caret)
-
-setwd('C:/Users/akshit/Desktop/Data Analytics Challenge/Blood Donation Challenge')
-getwd()
-
-#loading training and testing dataset
-blood_train_or <- read.csv('blood-train.csv')
-blood_test_or <- read.csv('blood-test.csv')
-
-View(blood_test_or)
-blood_train <- blood_train_or
-blood_test <- blood_test_or
-
-blood_train$X <- NULL
-blood_test$X <- NULL
+library(MLmetrics)
+blood_train <- read.csv('blood-train.csv')
+blood_test <- read.csv('blood-test.csv')
 
 
-str(blood_train)
-
-names <- c('MonthsLast','Number','Volume','MonthsFirst','Donated')
+names <- c('X','MonthsLast','Number','Volume','MonthsFirst','Donated')
 colnames(blood_train) <- names
-colnames(blood_test) <- names[1:4]
+colnames(blood_test) <- names[1:5]
 
 head(blood_test)
 head(blood_train)
 
-blood_test$Donated <- NA
-str(blood_test)
-#check that no datapoint is missing, otherwise we need to fix the dataset
-apply(blood_train,2,function(x) sum(is.na(x)))
-apply(blood_test,2,function(x) sum(is.na(x)))
 
+#blood_train$Donated <- as.factor(blood_train$Donated)
 
-
-blood_train$isTrainData <- TRUE
-blood_test$isTrainData <- FALSE
-
-
-
-
-blood_full <- rbind(blood_train, blood_test)
+blood_train$Volume <- NULL
 dim(blood_train)
+
+blood_train$Frequency <- log(blood_train$MonthsFirst / blood_train$Number)
+blood_train$MonthLFRatio <- log((blood_train$MonthsLast/(blood_train$MonthsFirst))+1)
+blood_train$NumbByFirst <- log(blood_train$Number/(blood_train$MonthsFirst)+1)
+blood_train$NumbByLast <- log((blood_train$Number/(blood_train$MonthsLast+1))+1)
+
+
+head(blood_test)
 dim(blood_test)
-dim(blood_full)
 
-range01 <- function(x)
-{
-  (x-min(x))/(max(x)- min(x))
-}
+blood_test$Volume <- NULL
+dim(blood_test)
+names(blood_test)
 
-df <- data.frame(c(1,2,3,4,5,6,7,8,9,10))
-range01(df)
-
-head(blood_full)
-
-# Normalizing the Predictor Variables 
-
-blood_full$Volume <- range01(blood_full$Volume)
-blood_full$Number <- range01(blood_full$Number)
-blood_full$MonthsLast <- range01(blood_full$MonthsLast)
-blood_full$MonthsFirst<- range01(blood_full$MonthsFirst)
-
-head(blood_full)
-
-# Correlations of each Variable Towards 'Donated' Variable'
-
-# cor(as.numeric(blood_train$MonthsFirst),as.numeric(blood_train$Donated))
-# cor(as.numeric(blood_train$MonthsLast),as.numeric(blood_train$Donated))
-# cor(as.numeric(blood_train$Volume),as.numeric(blood_train$Donated))
-# cor(as.numeric(blood_train$Number),as.numeric(blood_train$Donated))
+blood_test$Frequency <- log(blood_test$MonthsFirst / blood_test$Number)
+blood_test$MonthLFRatio <- log((blood_test$MonthsLast/(blood_test$MonthsFirst))+1)
+blood_test$NumbByFirst <- log(blood_test$Number/(blood_test$MonthsFirst)+1)
+blood_test$NumbByLast <- log((blood_test$Number/(blood_test$MonthsLast+1))+1)
 
 
 
-blood_full$Donated <- as.factor(blood_full$Donated)
+head(blood_test)
+blood_train <- blood_train[c(1:4,6:9,5)]    
+head(blood_train)
 
-#now extract back Train and Test dataset from full dataset
-#to identify train and test data we use "isTrainData" variable value
-blood_train <- blood_full[blood_full$isTrainData==TRUE, ]
-blood_test <- blood_full[blood_full$isTrainData==FALSE, ]
+#log -> numbyfirst,numbylast,  MonthLFRatio, frequenccy
+cor_mat <- cor(blood_train)
+cor_mat <- cor_mat[9,]
+View(cor_mat)
 
+# 
+# blood_train$Number <- log(blood_train$Number)
+# blood_train$MonthsLast <- log(2+blood_train$MonthsLast)
+# blood_train$MonthsFirst<- log(2+blood_train$MonthsFirst)
+# blood_train$MonthLFRatio <- log((blood_train$MonthsLast/(blood_train$MonthsFirst))+1)
+# blood_train$NumbByLast <- log(blood_train$Number/(blood_train$MonthsLast+1))+1
 
-blood_train$isTrainData <- NULL
-blood_test$isTrainData <- NULL
+View(blood_train)
 
-#fitting a random forest model
-
+set.seed(123)
+ind <- sample(2,nrow(blood_train),replace=TRUE, prob = c(0.8,0.2))
+train_data <- blood_train[ind==1,]
+dev_data <- blood_train[ind==2,]
+head(train_data)
+head(dev_data)
+dim(train_data)
+dim(dev_data)
+names(dev_data)
 str(blood_train)
 library(randomForest)
 #now first defined formula for predictiion of "Survived"
-Donated_equation <- "Donated ~ MonthsLast + Number + Volume + MonthsLast"
+Donated_equation <- 'Donated ~ NumbByLast + NumbByFirst + Number + Frequency + MonthLFRatio + MonthsFirst'
 Donated_formula <- as.formula(Donated_equation)
 
 str(Donated_formula)
-
-blood_model <- randomForest(formula= Donated_formula, 
-                            data= blood_train, 
-                            ntree = 2000, mtry = 1, 
-                            nodesize = 12,
+train_data$Donated <- as.factor(train_data$Donated)
+dev_data$Donated <- as.factor(dev_data$Donated) 
+blood_model <- randomForest(formula= Donated_formula,
+                            data= train_data,
+                            ntree = 1200, mtry = 4,
+                            nodesize = 21,
                             do.trace=TRUE)
-plot(blood_model)
 
 
-#prediction on test dataset
-View(blood_test)
-
-Donated_prediction <- predict(blood_model, newdata = blood_test,type='prob')
-head(Donated_prediction)
 
 
-f <- data.frame(Donated_prediction)
-class(f)
-head(f)
-View(f)
+table(dev_data$Donated)
 
 
-output <- cbind(blood_test_or$X,f$X1)
 
+Donated_prediction <- predict(blood_model, newdata = dev_data,type='class')
 
-df <- as.data.frame(output)
+# class(Donated_prediction)
+# head(Donated_prediction)
+df <- as.data.frame(Donated_prediction[,2])
 head(df)
-colnames(df)[1] <- ''
-head(output)
-View(output)
-write.csv(output, file = "Predictions.csv", row.names = FALSE)
+df <- cbind(blood_test$X,df$`Donated_prediction[, 2]`)
+df <- as.data.frame(df)
+head(df)
+names(df) <- c('','Made Donation in March 2007')
+write.csv(df,'Prediction.csv',row.names = FALSE)
 
 
+class(Donated_prediction)
+cm <- confusionMatrix(Donated_prediction, as.factor(dev_data$Donated))
+cm
+table(dev_data$Donated)
+cm$byClass
 ######################## RANDOM PARAMETER SEARCH FOR TUNING THE RANDOM FOREST ################################
 
 #Best OOB Error: 20.31%
 
-control <- trainControl(method="repeatedcv", number=10, repeats=3,search = 'random')
+control <- trainControl(method="repeatedcv", number=5, repeats=3,search = 'random')
 seed <- 123
 metric <- "Accuracy"
 set.seed(seed)
-mtry <- sqrt(ncol(blood_test))
+dev_data$Donated <- as.factor(dev_data$Donated)
+mtry <- sqrt(ncol(dev_data))
 tunegrid <- expand.grid(.mtry=mtry)
-rf_default <- train(Donated~., data=blood_train, method="rf", metric=metric, tuneLength=12, trControl=control,ntree=1000,do.trace=TRUE,nodesize=4,importance=TRUE, proximity= TRUE)
+dev_data$X<- NULL
+rf_default <- train(Donated~., data=dev_data, method="rf", metric = 'Kappa', tuneLength=12, trControl=control,ntree=1000,do.trace=TRUE,nodesize=4,importance=TRUE, proximity= TRUE)
 print(rf_default)
 attributes(rf_default)
-
+rf_default
 
 
